8c6ee5146eab20e1a23bf28a6076ef54733edf66
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-12-25 10:25:40 +0600

    part-16 some modification of frontend template

--- resources/views/backend/user/master.blade.php
+++ resources/views/backend/user/master.blade.php
@@ -59,47 +59,12 @@
 
             <div class="container-fluid">
 
-                <div class="breadcrumb-content d-flex flex-wrap align-items-center justify-content-between mb-5">
-                    <div class="media media-card align-items-center">
-                        <div class="media-img media--img media-img-md rounded-full">
-                            <img class="rounded-full" src="{{asset('frontend/images/small-avatar-1.jpg')}}" alt="Student thumbnail image">
-                        </div>
-                        <div class="media-body">
-                            <h2 class="section__title fs-30">Howdy, Tim Buchalka</h2>
-                            <div class="rating-wrap d-flex align-items-center pt-2">
-                                <div class="review-stars">
-                                    <span class="rating-number">4.4</span>
-                                    <span class="la la-star"></span>
-                                    <span class="la la-star"></span>
-                                    <span class="la la-star"></span>
-                                    <span class="la la-star"></span>
-                                    <span class="la la-star-o"></span>
-                                </div>
-                                <span class="rating-total pl-1">(20,230)</span>
-                            </div><!-- end rating-wrap -->
-                        </div><!-- end media-body -->
-                    </div><!-- end media -->
-                    <div class="file-upload-wrap file-upload-wrap-2 file--upload-wrap">
-                        <input type="file" name="files[]" class="multi file-upload-input">
-                        <span class="file-upload-text"><i class="la la-upload mr-2"></i>Upload a course</span>
-                    </div><!-- file-upload-wrap -->
-                </div><!-- end breadcrumb-content -->
-                <div class="section-block mb-5"></div>
+                @include('backend.user.section.breadcrumb')
 
                 @yield('content')
 
-                <div class="row align-items-center dashboard-copyright-content pb-4">
-                    <div class="col-lg-6">
-                        <p class="copy-desc">&copy; 2021 Aduca. All Rights Reserved. by <a href="https://techydevs.com/">TechyDevs</a>
-                        </p>
-                    </div><!-- end col-lg-6 -->
-                    <div class="col-lg-6">
-                        <ul class="generic-list-item d-flex flex-wrap align-items-center fs-14 justify-content-end">
-                            <li class="mr-3"><a href="terms-and-conditions.html">Terms & Conditions</a></li>
-                            <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                        </ul>
-                    </div><!-- end col-lg-6 -->
-                </div><!-- end row -->
+                @include('backend.user.section.footer')
+
 
 
             </div>
